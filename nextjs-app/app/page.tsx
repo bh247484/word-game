4ea7e1d8ffb1e8ad0954f107c67ded046c7f4240
bh247484 <bh247484@gmail.com>
2023-03-26 20:46:29 -0400

    Add random letter state and render letters into grid components

--- nextjs-app/app/page.tsx
+++ nextjs-app/app/page.tsx
@@ -1,11 +1,19 @@
 'use client';
 
-import { useState, } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './page.module.css';
 import binSearch from './searchAlgo/binSearch';
+import { randomLetter } from './randGen/randGen'
+import LetterGrid from './components/letter-grid';
+
 
 export default function Home() {
   const [word, setWord]: [string, Function] = useState("");
+  const [letters, setLetters]: [string[], Function] = useState(
+    [...Array(20)].map(() => randomLetter())
+  );
+
+  // console.log([...Array(20)].map(() => randomLetter()));
 
   const submitWord = () => {
     if (word.length < 3) {
@@ -23,6 +31,9 @@ export default function Home() {
     setWord("");
   };
 
+  // useEffect(() => {
+  // }, [])
+
   return (
     <div className="App">
       <h1>Text</h1>
@@ -33,6 +44,9 @@ export default function Home() {
         onKeyUp={(e) => e.key === 'Enter' ? submitWord() : null}
       />
       <button onClick={submitWord}>Try</button>
+      <LetterGrid
+        letters={letters}
+      />
     </div>
   );
 }
