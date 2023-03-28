922b5d7aa94c4c84eff0f7303e7392b8d0276cf0
bh247484 <bh247484@gmail.com>
2023-03-28 16:37:20 -0400

    Add queuing logic, fix sanitization duplicate oversight

--- nextjs-app/app/page.tsx
+++ nextjs-app/app/page.tsx
@@ -5,12 +5,14 @@ import styles from './page.module.css';
 import binSearch from './searchAlgo/binSearch';
 import { randomLetter } from './randGen/randGen'
 import Grid from './components/grid';
+import { IBlock } from './types/types';
 
 
 export default function Home() {
   const [word, setWord]: [string, Function] = useState("");
-  const [columns, setColumns]: [string[][], Function] = useState(
-    Array.from(Array(6), () => [...Array(10)].map((_, i) => i > 3 ? '0' : randomLetter()))
+  const [columns, setColumns]: [IBlock[][], Function] = useState(
+    Array.from(Array(6), () => [...Array(10)].map(
+      (_, i) => i > 3 ? { queued: false, letter: '0' } : { queued: false, letter: randomLetter() }))
   );
 
   const submitWord = () => {
@@ -29,10 +31,40 @@ export default function Home() {
     setWord("");
   };
 
-  const changeHandler = (value: string) => {
-    const sanitizedStr = value.split('').filter((char) => {
-      return columns.flat().includes(char);
+  const queueLetters = (letters: string[]) => {
+    // Sort Columns by length to prioritize letters in longest arrays.
+    const locCols: IBlock[][] = columns;
+    locCols.sort((a,b) => b.length - a.length);
+
+    // Dequeue all letters first.
+    locCols.forEach((col) => {
+      col.forEach((block) => block.queued = false);
     });
+
+    letters.forEach((letter) => {
+      loop1: for (const col of locCols) {
+        for (const block of col) {
+          // && !block.queued to make sure we're not re-queuing the same letter more than once.
+          if (block.letter === letter && !block.queued) {
+            block.queued = true;
+            break loop1;
+          }
+        }
+      }
+    });
+    setColumns(locCols);
+  };
+
+  const changeHandler = (value: string) => {
+    // Filter out letters that are not in the grid.
+    let allowedLetters = columns.flat().map(({ letter }) => letter);
+    const sanitizedStr = value.split('').filter((char) => {
+      const bool = allowedLetters.includes(char);
+      // Remove first occurence of letter.
+      allowedLetters.splice(allowedLetters.findIndex(x => x === char), 1);
+      return bool;
+    });
+    queueLetters(sanitizedStr);
     setWord(sanitizedStr.join(''));
   };
 
