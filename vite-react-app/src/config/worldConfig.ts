211b6e6b8586a1a62888bd2f3db39775b8f60eb8
bh247484 <bh247484@gmail.com>
2023-05-20 09:19:09 -0400

    Update level interval config

--- vite-react-app/src/config/worldConfig.ts
+++ vite-react-app/src/config/worldConfig.ts
@@ -6,11 +6,11 @@ export const worldConfig: IWorldConfig[] = [
         levels: [
             {
                 dripDelay: 15 * 1000,
-                time: 60,
+                time: 45,
                 rows: 4
             },
             {
-                dripDelay: 15 * 1000,
+                dripDelay: 14 * 1000,
                 time: 60,
                 rows: 4
             }
@@ -20,14 +20,14 @@ export const worldConfig: IWorldConfig[] = [
         name: 'World 1',
         levels: [
             {
-                dripDelay: 15 * 1000,
-                time: 60,
+                dripDelay: 13 * 1000,
+                time: 75,
                 rows: 4
             },
             {
-                dripDelay: 15 * 1000,
-                time: 10,
-                rows: 4
+                dripDelay: 12 * 1000,
+                time: 75,
+                rows: 5
             }
         ]
     },
@@ -35,14 +35,14 @@ export const worldConfig: IWorldConfig[] = [
         name: 'World 2',
         levels: [
             {
-                dripDelay: 15 * 1000,
-                time: 60,
-                rows: 4
+                dripDelay: 11 * 1000,
+                time: 80,
+                rows: 5
             },
             {
-                dripDelay: 15 * 1000,
-                time: 60,
-                rows: 4
+                dripDelay: 10 * 1000,
+                time: 90,
+                rows: 6
             }
         ]
     },
