62f31a9e1760d76e87543904678b1a044f966f88
bh247484 <bh247484@gmail.com>
2023-05-21 12:31:41 -0400

    Test fetch to asp-api

--- vite-react-app/src/App.tsx
+++ vite-react-app/src/App.tsx
@@ -3,8 +3,15 @@ import styles from './app.module.css';
 import { GameBoard } from '@/components';
 
 function App() {
+  async function testFetch() {
+    const response = await fetch("http://localhost:5000/WordGame");
+    console.log(response);
+    const data = await response.text();
+    console.log(data);
+  }
   return (
     <div className={styles.container}>
+      <button onClick={testFetch}>Test Fetch</button>
       <GameBoard />
     </div>
   );
