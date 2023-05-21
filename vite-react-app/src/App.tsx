87ca98bf9b51bd479e82ff434d20fe437a9feb4c
bh247484 <bh247484@gmail.com>
2023-05-21 13:40:04 -0400

    Comment out test fetch button/function

--- vite-react-app/src/App.tsx
+++ vite-react-app/src/App.tsx
@@ -3,15 +3,15 @@ import styles from './app.module.css';
 import { GameBoard } from '@/components';
 
 function App() {
-  async function testFetch() {
-    const response = await fetch("http://localhost:3000/api/v1/tests/index");
-    console.log(response);
-    const data = await response.text();
-    console.log(data);
-  }
+  // async function testFetch() {
+  //   const response = await fetch("http://localhost:3000/api/v1/tests/index");
+  //   console.log(response);
+  //   const data = await response.text();
+  //   console.log(data);
+  // }
   return (
     <div className={styles.container}>
-      <button onClick={testFetch}>Test Fetch</button>
+      {/* <button onClick={testFetch}>Test Fetch</button> */}
       <GameBoard />
     </div>
   );
