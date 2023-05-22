7a24e7003a60a7285cf265a4f34b9bb993d18d43
bh247484 <bh247484@gmail.com>
2023-05-22 11:14:51 -0400

    Remove test fetch

--- vite-react-app/src/App.tsx
+++ vite-react-app/src/App.tsx
@@ -2,15 +2,8 @@ import styles from './app.module.css';
 import { GameBoard } from '@/components';
 
 function App() {
-  // async function testFetch() {
-  //   const response = await fetch("http://localhost:3000/api/v1/tests/index");
-  //   console.log(response);
-  //   const data = await response.text();
-  //   console.log(data);
-  // }
   return (
     <div className={styles.container}>
-      {/* <button onClick={testFetch}>Test Fetch</button> */}
       <GameBoard />
     </div>
   );
