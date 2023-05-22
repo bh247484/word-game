434ce77d42209d7f603fdd53b01d1edf99e18bb0
bh247484 <bh247484@gmail.com>
2023-05-22 11:15:12 -0400

    Implement high score frontend functionality

--- vite-react-app/src/components/end-game/index.tsx
+++ vite-react-app/src/components/end-game/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { worldConfig } from "@/config/worldConfig";
 import styles from './end-game.module.css';
 
@@ -8,10 +9,54 @@ interface IProps {
 }
 
 export default function EndGame({ gameScore, dispatches, scoredWords }: IProps) {
+  const [highScores, setHighScores]:[{ name:string, score:number }[], Function] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [newHighScore, setNewHighScore] = useState(false);
+  const [name, setName] = useState('')
   const [gcDispatch, gridDispatch, scoreDispatch] = dispatches;
-  // Make API request for High Scores.
 
-  // ...
+  async function getHighScores() {
+    const response = await fetch("http://localhost:3000/api/v1/high_scores");
+    const scores = await response.json();
+    setHighScores(scores);
+
+    // Determine if new high score set.
+    if (scores.length < 10 || scores[scores.length - 1].score < gameScore) {
+      setNewHighScore(true);
+    }
+    setLoading(false);
+  }
+
+  async function submitNewScore(name: string) {
+    // Update local, frontend high scores.
+    const updatedScores = [...highScores];
+    const newScore = {
+      name,
+      score: gameScore,
+    }
+    if (updatedScores.length === 10) {
+      updatedScores.splice(-1);
+    }
+    updatedScores.push(newScore);
+    updatedScores.sort((a,b) => b.score - a.score);
+    setHighScores(updatedScores);
+
+    // Update backend db high scores.
+    const response = await fetch('http://localhost:3000/api/v1/high_scores', {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newScore),
+    })
+    setNewHighScore(false);
+  }
+  
+  // Make API request for High Scores.
+  useEffect(() => {
+    getHighScores();
+  }, [])
 
   const newGame = () => {
     gcDispatch({ type: 'reset' });
@@ -23,6 +68,34 @@ export default function EndGame({ gameScore, dispatches, scoredWords }: IProps) 
     <div className={styles['wrapper']}>
       <h2>Game Over</h2>
       <h3>Your Score: {gameScore}</h3>
+      {
+        !loading ? (
+          newHighScore ? (
+            <div>
+              <h4>New High Score!</h4>
+              <p>Enter Your Name</p>
+              <input
+                className={styles.input}
+                maxLength={3}
+                type="text"
+                value={name}
+                onChange={({ target }) => setName(target.value.toUpperCase())}
+                onKeyUp={({ key }) => key === 'Enter' ? submitNewScore(name) : null}
+              />
+              <button onClick={() => submitNewScore(name)}>Submit</button>
+            </div>
+          ) : (
+            <>
+              <h2>High Scores</h2>
+              {
+                highScores.map((entry) => (
+                  <li>{entry.name} -- {entry.score}</li>
+                ))
+              }
+            </>
+          )
+        ) : null
+      }
       <button onClick={newGame}>Start New Game</button>
     </div>
   );
